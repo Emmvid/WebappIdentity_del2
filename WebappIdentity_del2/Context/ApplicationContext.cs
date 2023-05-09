4440173b10c058a3743bb7c5573716b28eb86076
Emmvid <emmavidarsson@hotmail.com>
2023-05-09 14:39:11 +0200

    Updated contexts

---
+++ WebappIdentity_del2/Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebappIdentity_del2.Models.Entities;
+
+namespace WebappIdentity_del2.Context;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
+ 
+
+    public DbSet<ProductEntity> Products { get; set; }
+    public DbSet<CategoryEntity> Categories { get; set; }
+    public DbSet<ProductCategoryEntity> ProductCategories { get; set; }
+    public DbSet<ContactFormEntity> ContactForms { get; set; }
+
+
+}
