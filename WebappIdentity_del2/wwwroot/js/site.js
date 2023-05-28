0e0be1f2aeb7f2a4cbba699667fffc3bcca80192
Emmvid <emmavidarsson@hotmail.com>
2023-05-28 20:41:01 +0200

    Final

--- WebappIdentity_del2/wwwroot/js/site.js
+++ WebappIdentity_del2/wwwroot/js/site.js
@@ -10,27 +10,6 @@ function footerPosition(element, scrollHeight, innerHeigt) {
     catch { }
 }
 
-/*function toggleMenu(attribute) {
-    try {
-        const toggleBtn = document.querySelector(attribute)
-        toggleBtn.addEventListener('click', function () {
-            const element = document.querySelector(toggleBtn.getAttribute('data-target'))
-
-            if (!element.classList.contains('open-menu')) {
-                element.classList.add('open-menu')
-                toggleBtn.classList.add('btn-outline-dark')
-                toggleBtn.classList.add('btn-toggle-white')
-            }
-
-            else {
-                element.classList.remove('open-menu')
-                toggleBtn.classList.remove('btn-outline-dark')
-                toggleBtn.classList.remove('btn-toggle-white')
-            }
-        })
-    } catch { }
-
-}*/
 
 //Anropar funktionerna
 try {
@@ -52,18 +31,5 @@ try {
     })
 } catch { }
 footerPosition('footer', document.body.scrollHeight, window.innerHeight)
-//toggleMenu('[data-option="toggle"')
 
 
-// validating contact form
-
-const email = document.querySelector(".email");
-console.log(email)
-
-email.addEventListener("input", (event) => {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("I am expecting an email address!");
-    } else {
-        email.setCustomValidity("");
-    }
-});
